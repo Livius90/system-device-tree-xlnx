Livius90/system-device-tree-xlnx

commit ff560663ec3873d58dfc42ec2bc78edb83c71409

clk_wiz: Sync the DTG changes to avoid the probe error
Linux is expecting "xlnx,nr-outputs" property in clk_wiz node. Without this, all
the other drivers where clk_wiz is referred as the clock will not be probed. This
was added to DTG few months back with below commit:
7ecf60401f4c : clk_wiz: Add nr-outputs and rename speed-grade.
Sync the same to SDT repo.

Signed-off-by: Onkar Harsh <onkar.harsh@amd.com>
Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@amd.com>
Acked-by: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

axi_clk_wiz/data/axi_clk_wiz.tcl
@@ -14,10 +14,6 @@
 #
 
     proc axi_clk_wiz_generate {drv_handle} {
-        global env
-        global dtsi_fname
-        set path $env(REPO)
-
         set node [get_node $drv_handle]
         if {$node == 0} {
                 return
@@ -25,6 +21,7 @@
         set keyval [pldt append $node compatible "\ \, \"xlnx,clocking-wizard\""]
         set ip [hsi::get_cells -hier $drv_handle]
         axi_clk_wiz_gen_speedgrade $drv_handle
+        set j 0
         set output_names ""
         for {set i 1} {$i < 8} {incr i} {
                 if {[hsi get_property CONFIG.C_CLKOUT${i}_USED $ip] != 0} {
@@ -33,17 +30,15 @@
                         set basefrq [string tolower [hsi get_property CONFIG.C_BASEADDR $ip]]
                         set pin_name "$basefrq-$pin_name"
                         lappend output_names $pin_name
+                        incr j
                 }
         }
         if {![string_is_empty $output_names]} {
                 add_prop $node "clock-output-names" $output_names string "pl.dtsi"
+                add_prop $node "xlnx,nr-outputs" $j int "pl.dtsi"
         }
         add_prop $node "#clock-cells" 1 int "pl.dtsi"
-
-        set family [get_hw_family]
-        if {[regexp "microblaze" $family match]} {
-                gen_dev_ccf_binding $drv_handle "clk_in1 s_axi_aclk" "clocks clock-names"
-        }
+        gen_dev_ccf_binding $drv_handle "clk_in1 s_axi_aclk" "clocks clock-names"
     }
 
     proc axi_clk_wiz_gen_speedgrade {drv_handle} {
@@ -51,10 +46,6 @@
         set num [regexp -all -inline -- {[0-9]} $speedgrade]
         if {![string equal $num ""]} {
                 set node [get_node $drv_handle]
-                add_prop $node "speed-grade" $num int "pl.dtsi"
+                add_prop $node "xlnx,speed-grade" $num int "pl.dtsi"
         }
     }
-
-    set connected_ip 0
-
-

device_tree/data/device_tree.tcl
@@ -44,6 +44,7 @@ proc init_proclist {} {
 	dict set ::sdtgen::namespacelist "canfd" "axi_can"
 	dict set ::sdtgen::namespacelist "axi_cdma" "axi_cdma"
 	dict set ::sdtgen::namespacelist "clk_wiz" "axi_clk_wiz"
+	dict set ::sdtgen::namespacelist "clk_wizard" "axi_clk_wiz"
 	dict set ::sdtgen::namespacelist "axi_dma" "axi_dma"
 	dict set ::sdtgen::namespacelist "axi_emc" "axi_emc"
 	dict set ::sdtgen::namespacelist "axi_ethernet" "axi_ethernet"

commit 3476c78ced0e611877052cf81325bbdd8b4fe07d

clk_wiz: Update the clock generation
Currently the device tree is generated twice once from the
driver tcl and once from the devicetree tcl. gen_dev_ccf_binding is intended
for microblaze designs. Add a check in the call to teh function for
microblaze

Fixes: ff560663 ("clk_wiz: Sync the DTG changes to avoid the probe error")
Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@amd.com>

Acked-by : Onkar Harsh <onkar.harsh@amd.com>

axi_clk_wiz/data/axi_clk_wiz.tcl
@@ -1,6 +1,6 @@
 #
 # (C) Copyright 2014-2022 Xilinx, Inc.
-# (C) Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+# (C) Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -38,7 +38,11 @@
                 add_prop $node "xlnx,nr-outputs" $j int "pl.dtsi"
         }
         add_prop $node "#clock-cells" 1 int "pl.dtsi"
-        gen_dev_ccf_binding $drv_handle "clk_in1 s_axi_aclk" "clocks clock-names"
+
+        set family [get_hw_family]
+        if {[regexp "microblaze" $family match]} {
+                gen_dev_ccf_binding $drv_handle "clk_in1 s_axi_aclk" "clocks clock-names"
+       }
     }
 
     proc axi_clk_wiz_gen_speedgrade {drv_handle} {